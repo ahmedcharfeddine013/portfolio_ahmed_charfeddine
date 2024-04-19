abfe4b687d0f28874569607f60a9b8144774c104
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-19 12:59:00 +0100

    dashboard card finished

--- src/app/admin/(root)/page.tsx
+++ src/app/admin/(root)/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import db from "@/db/db";
+import Image from "next/image";
+import React from "react";
+
+function getProducts() {
+  return db.project.findMany({
+    where: { published: true },
+    orderBy: { createdAt: "desc" },
+  });
+}
 
 export default function AdminPage() {
+  return <div></div>;
+}
+
+function ProjectCard({
+  name,
+  githubLink,
+  imagePath,
+}: {
+  name: string;
+  githubLink: string;
+  imagePath: string;
+}) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Card>
+      <div>
+        <Image src={imagePath} alt={name} height={300} width={300} />
+      </div>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{githubLink}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
 }
