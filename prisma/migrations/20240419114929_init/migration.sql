2e9c80d84634531b2fddf3e0a1c3f7384da6b955
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-19 12:50:33 +0100

    model setup

---
+++ prisma/migrations/20240419114929_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "descripion" TEXT NOT NULL,
+    "imagePath" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "githubLink" TEXT NOT NULL
+);
