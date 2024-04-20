66b2a55bcdd943c5d4df9be034c94bb2ed8f0e03
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-20 16:38:09 +0100

    parallax fixed

--- src/app/(userRoute)/(root)/page.tsx
+++ src/app/(userRoute)/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-[200vh]">
       <Hero />
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,30 +3,34 @@ import React from "react";
 import { MouseParallax } from "react-just-parallax";
 import Image from "next/image";
 import hero_bg from "../../public/bg.jpg";
+import { ScrollParallax } from "react-just-parallax";
 
 export default function Hero() {
   return (
     <div className=" max-w-full w-full h-[500px] flex flex-col  items-center justify-center ">
-      <div className="text-center flex items-center justify-center flex-col space-y-4 z-50">
-        <h1 className="text-3xl ">
-          Hi, I am <span className="text-blue-400">Ahmed Charfeddine</span>
-        </h1>
-        <p>
-          A seasoned <span className="text-blue-400">Frontend Developer</span>{" "}
-          transforming ideas into stunning digital experiences. Let&apos;s
-          create something amazing!{" "}
-        </p>
-      </div>
+      <ScrollParallax strength={0.1}     >
+        <div className="text-center flex items-center justify-center flex-col space-y-4 h-full z-50">
+          <h1 className="text-3xl">
+            Hi, I am <span className="text-blue-400">Ahmed Charfeddine</span>
+          </h1>
+          <p>
+            A seasoned <span className="text-blue-400">Frontend Developer</span>{" "}
+            transforming ideas into stunning digital experiences. Let&apos;s
+            create something amazing!{" "}
+          </p>
+        </div>
+      </ScrollParallax>
       <div className=" h-[500px] overflow-hidden  w-screen">
         <div className="absolute h-[500px]  overflow-hidden left-0 top-[0px] w-screen text-violet-700 z-[-50]">
           <MouseParallax strength={0.05}>
             <Image
               src={hero_bg}
-              className="w-full scale-110"
+              className="w-full scale-125 opacity-50"
               alt="hero"
               height={2000}
             ></Image>
           </MouseParallax>
+          <div className="absolute bottom-0 bg-gradient-to-t from-background to-transparent h-24 w-full"></div>
         </div>
       </div>
     </div>
