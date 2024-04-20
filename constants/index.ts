41222935acf65e290cae91125315707e4737bbfb
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-20 23:28:48 +0100

    skills setup

---
+++ constants/index.ts
@@ -0,0 +1,53 @@
+import html from "../assets/html-1.svg";
+import css from "../assets/css-3.svg";
+import js from "../assets/logo-javascript.svg";
+import ts from "../assets/typescript.svg";
+import reactjs from "../assets/react-2.svg";
+import nextjs from "../assets/next-js.svg";
+import expressjs from "../assets/express-109.svg";
+import nodejs from "../assets/nodejs-3.svg";
+import prisma from "../assets/prisma-4.svg";
+import mongoDb from "../assets/mongodb-icon-1.svg";
+
+export const skills = [
+  {
+    skill: "html",
+    image: html,
+  },
+  {
+    skill: "css",
+    image: css,
+  },
+  {
+    skill: "javascript",
+    image: js,
+  },
+  {
+    skill: "typescript",
+    image: ts,
+  },
+  {
+    skill: "react",
+    image: reactjs,
+  },
+  {
+    skill: "nextJs",
+    image: nextjs,
+  },
+  {
+    skill: "expressJs",
+    image: expressjs,
+  },
+  {
+    skill: "nodeJs",
+    image: nodejs,
+  },
+  {
+    skill: "Prisma",
+    image: prisma,
+  },
+  {
+    skill: "mongoDB",
+    image: mongoDb,
+  },
+];
