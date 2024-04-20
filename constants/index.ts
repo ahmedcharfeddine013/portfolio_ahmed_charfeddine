658098093107f69be109b16c52d752248f513509
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-21 00:23:05 +0100

    landing page fixed

--- constants/index.ts
+++ constants/index.ts
@@ -4,17 +4,12 @@ import js from "../assets/logo-javascript.svg";
 import ts from "../assets/typescript.svg";
 import reactjs from "../assets/react-2.svg";
 import nextjs from "../assets/next-js.svg";
-import expressjs from "../assets/express-109.svg";
 import nodejs from "../assets/nodejs-3.svg";
 import prisma from "../assets/prisma-4.svg";
 import mongoDb from "../assets/mongodb-icon-1.svg";
 import materialUi from "../assets/material-ui-1.svg";
 import tailwindCss from "../assets/tailwind-css-2.svg";
-import bootstrap from "../assest/bootstrap-4.svg";
-
-// import { Linkedin } from "lucide-react";
-// import { Github } from "lucide-react";
-// import { Mail } from "lucide-react";
+import bootstrap from '../assets/bootstrap-4.svg'
 
 const skills = [
   {
@@ -66,19 +61,5 @@ const skills = [
     image: mongoDb,
   },
 ];
-
-// const SocialMedia = [
-//   {
-//     title: "Github",
-//     icon: Github,
-//     link: "https://github.com/ahmedcharfeddine013",
-//   },
-//   {
-//     title: "Linkedin",
-//     icon: Linkedin,
-//     link: "https://www.linkedin.com/in/ahmed-charfeddine-677156245/",
-//   },
-//   { title: "E-mail", icon: Mail, link: "ahmedcharfeddine75@gmail.com" },
-// ];
 
 export { skills };
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { skills } from "../../constants";
 import Image from "next/image";
+import PageHeader from "./PageHeader";
 export default function Skills() {
   return (
-    <div className="flex flex-row flex-wrap items-center justify-center space-x-5">
-      {skills.map((skill) => (
-        <div
-          key={skill.skill}
-          className=" flex items-center justify-center rounded-full bg-white border-blue-400 h-[100px] w-[100px] border-2"
-        >
-          <Image src={skill.image} alt={skill.skill} height={50} width={50} />
-        </div>
-      ))}
+    <div className="flex items-center justify-center flex-col space-y-10">
+      <PageHeader>Skills</PageHeader>
+      <div className="flex flex-row flex-wrap items-center justify-center gap-5">
+        {skills.map((skill) => (
+          <div
+            key={skill.skill}
+            className=" flex items-center justify-center rounded-full bg-white border-blue-400 h-[100px] w-[100px] border-2"
+          >
+            <Image src={skill.image} alt={skill.skill} height={50} width={50} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
