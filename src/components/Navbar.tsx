7217778242fd2b1ddca659c37159909157f3e726
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-23 23:50:13 +0100

    navbar bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,6 @@ export default function Navbar() {
   useEffect(() => {
     const handleResize = () => {
       const newWidth = window.innerWidth;
-      // console.log(newWidth);
       if (newWidth > 1024) {
         setHide(true);
       } else {
@@ -23,15 +22,17 @@ export default function Navbar() {
     };
 
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolling(true);
-      } else {
+      if (window.scrollY === 0) {
         setScrolling(false);
+      } else {
+        setScrolling(true);
       }
     };
 
     window.addEventListener("resize", handleResize);
+    handleResize();
     window.addEventListener("scroll", handleScroll);
+    // handleScroll(); // You can choose to omit this call if you want the initial state to be set based on user's scroll behavior.
     return () => {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("scroll", handleScroll);
@@ -39,10 +40,10 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className="flex flex-row space-x-4 fixed w-screen justify-center lg:justify-between items-center z-50 ">
+    <nav className={`flex flex-row space-x-4 fixed w-screen justify-center lg:justify-between items-center z-50 duration-100 transition-all ease-in ${scrolling && hide ? 'bg-gray-900/80 backdrop-blur-md' : 'bg-transparent'}`}>
       <div
-        className={`absolute lg:relative flex flex-col w-84 space-y-8 lg:flex-row    h-screen top-0 left-0  bg-opacity-20 lg:h-full lg:bg-transparent lg:space-y-0 lg:justify-between lg:w-full justify-center items-center text-center lg:items-center lg:py6 py-8 px-8  ease-in transition-transform duration-300  ${
-          toggled ? "translate-x-0" : "translate-x-[-100%] lg:translate-x-0"
+        className={`absolute lg:relative flex flex-col w-84 space-y-8 lg:flex-row h-screen top-0 left-0  bg-opacity-20 lg:h-full lg:bg-transparent lg:space-y-0 lg:justify-between lg:w-full justify-center items-center text-center lg:items-center lg:py6 py-8 px-8  ease-in transform transition-all duration-100  ${
+          toggled ? "translate-x-0 bg-gray-900/80 backdrop-blur-md" : "translate-x-[-100%] lg:translate-x-0 bg-transparent"
         }`}
       >
         <ul className="flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-8 text-xl">
