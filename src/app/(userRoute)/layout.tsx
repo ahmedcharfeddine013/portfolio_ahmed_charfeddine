9d57ba2323d39e6913ce495f9a7aa7732e330f06
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-23 09:24:49 +0100

    icon

--- src/app/(userRoute)/layout.tsx
+++ src/app/(userRoute)/layout.tsx
@@ -1,6 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export const dynamic = "force-dynamic";
+
+export const metadata = {
+  icons: {
+    icon: "/Logo.png", // /public path
+  },
+};
 
 export default function Layout({
   children,
@@ -9,13 +16,13 @@ export default function Layout({
 }>) {
   return (
     <>
+      {/* <Head>
+        <link rel="icon" href="/Logo.png" sizes="64x64" type="image/png" />
+      </Head> */}
       <div className="w-screen">
         <Navbar />
         {/* <Nav /> */}
-        <div className="px-10">
-
-        {children}
-        </div>
+        <div className="px-10">{children}</div>
       </div>
     </>
   );
