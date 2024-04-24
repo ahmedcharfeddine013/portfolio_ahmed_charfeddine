73c9e24beed30ebce075807d1a578766d4baca32
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-24 15:49:49 +0100

    hero fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,42 +8,6 @@ import SocialMedia from "./SocialMedia";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
-  const [name, setName] = useState("");
-  const fullName = "Ahmed Charfeddine";
-  const typingSpeed = 150; // Adjust typing speed as needed
-  const eraseSpeed = 50; // Adjust erasing speed as needed
-
-  useEffect(() => {
-    const typeWriter = () => {
-      let i = 0;
-      const type = () => {
-        if (i < fullName.length) {
-          setName((prevName) => prevName + fullName.charAt(i));
-          i++;
-          setTimeout(type, typingSpeed);
-        } else {
-          setTimeout(erase, 1000);
-        }
-      };
-
-      const erase = () => {
-        if (i >= 0) {
-          setName((prevName) => prevName.slice(0, -1));
-          i--;
-          setTimeout(erase, eraseSpeed);
-        } else {
-          setTimeout(type, 1000);
-        }
-      };
-
-      type();
-    };
-
-    typeWriter();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className=" w-full h-[500px] z-10 flex flex-col items-center justify-center ">
       <ScrollParallax strength={0.1}>
